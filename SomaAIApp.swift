eab99968b1f56aac095196c93e79af75b8f1f7c1
Олег <oleg@example.com>
2026-06-23 09:50:48 +0300

    test: absolute sterile run - hello world

--- SomaAIApp.swift
+++ SomaAIApp.swift
@@ -1,13 +1,11 @@
 import SwiftUI
-import SwiftData
 
 @main
 struct SomaAIApp: App {
     var body: some Scene {
         WindowGroup {
-            MainTabView()
-            // Temporarily disabling modelContainer to isolate the crash
-            // .modelContainer(for: [UserProfile.self, LabTest.self, LabMarker.self, Medication.self, HealthEvent.self, Allergy.self, Condition.self])
+            Text("Hello World - Sterile Test")
+                .font(.largeTitle)
         }
     }
 }
